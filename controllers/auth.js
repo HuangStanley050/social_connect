3d238a705df875aabc71adc1a4713edfb15dd0c1
Stanley Huang <infamous_godhand@yahoo.com>
2018-12-13 07:52:44 +0000

    register route done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,39 @@
 const User = require("../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 exports.register = (req, res, next) => {
-  User.findOne({ email: req.body.email })
-    .then()
-    .catch();
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exist" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar: avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+          console.log(err);
+        }
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) {
+            throw err;
+          }
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json({ user: user }))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
 };

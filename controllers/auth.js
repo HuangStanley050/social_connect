66808d8f58a54fc43b4022d3e3d4c28f51b7dcc8
Stanley Huang <infamous_godhand@yahoo.com>
2018-12-14 04:16:46 +0000

    register validation done

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,7 +15,8 @@ exports.register = (req, res, next) => {
 
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exist" });
+      errors.email = "Email already exist";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200",
--- validation/register.js
+++ validation/register.js
@@ -5,8 +5,35 @@ const isEmpty = require("./is-empty");
 exports.validateRegisterInput = data => {
   let errors = {};
 
+  //check if the data is empty if it is convert to empty string
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
+  }
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name can't be empty";
+  }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email can't be empty";
+  }
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Not a valid email";
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password can't be empty";
+  }
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be between 6 and 30 characters";
+  }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
+  }
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Password must match";
   }
   return {
     errors: errors,

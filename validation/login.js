52c442d80ff56818a3f330e9cc41397f1ce1710a
Stanley Huang <infamous_godhand@yahoo.com>
2018-12-14 04:35:26 +0000

    added login validation

---
+++ validation/login.js
@@ -0,0 +1,27 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+//import isEmpty from "./is-empty";
+
+exports.validateRegisterInput = data => {
+  let errors = {};
+
+  //check if the data is empty if it is convert to empty string
+
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email can't be empty";
+  }
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Not a valid email";
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password can't be empty";
+  }
+
+  return {
+    errors: errors,
+    isValid: isEmpty(errors)
+  };
+};

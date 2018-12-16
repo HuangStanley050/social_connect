0796405e64b72b6768c5bb71df9f30757d20df50
Stanley Huang <infamous_godhand@yahoo.com>
2018-12-16 02:39:51 +0000

    finished get current profile route and controller

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const authController = require("../../controllers/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
 const router = express.Router();
 
 // @GET /api/profile/test
@@ -8,5 +14,15 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({ message: "profile route" });
 });
+
+// @GET /api/profile
+// @desc get current user profile
+// @access private
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  authController.currentProfile
+);
 
 module.exports = router;
